7e66c34d49495b96a92dcee4929dc323b18e9ea4
David Malcolm <dave_malcolm@src.gnome.org>
2003-12-01 03:17:45 +0000

    Cleanups of source

--- src/file.c
+++ src/file.c
@@ -7,37 +7,60 @@
 #include "global.h"
 #include "cong-file-selection.h"
 
+/*
+  This file isolates the API change-over between gtk_file_selection and gtk_file_chooser.
+ */
 #if 0
-/* Implementation in terms of new GtkFileChooser interface: */
-gchar *cong_get_file_name(const gchar *title, 
-			  const gchar *filename,
-			  GtkWindow *parent_window,
-			  CongFileChooserAction action)
+/* Implementation in terms of gtk_file_chooser: */
+gchar*
+cong_get_file_name (const gchar *title, 
+		    const gchar *filename,
+		    GtkWindow *parent_window,
+		    CongFileChooserAction cong_action)
 {
-	GtkWidget *dialog;
 	gchar *result = NULL;
+	GtkWidget *dialog;
 	GtkFileChooserAction gtk_action;
 
 	g_return_val_if_fail (title, NULL);
 	g_return_val_if_fail (parent_window, NULL);
 
-	switch (action) {
-	default: g_assert_not_reached();
-	case GTK_FILE_CHOOSER_ACTION_OPEN:
+	switch (cong_action) {
+	default: g_assert_not_reached ();
+	case CONG_FILE_CHOOSER_ACTION_OPEN:
 		gtk_action = GTK_FILE_CHOOSER_ACTION_OPEN;
 		break;
-	case GTK_FILE_CHOOSER_ACTION_SAVE:
-		gtk_action = GTK_FILE_CHOOSER_ACTION_SAVE;
-		break;		
-	}
 
+	case CONG_FILE_CHOOSER_ACTION_SAVE:
+		gtk_action = GTK_FILE_CHOOSER_ACTION_SAVE;
+		break;
+	}
+	
 	dialog = gtk_file_chooser_dialog_new (title,
 					      parent_window,
 					      gtk_action,
 					      GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 					      GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
 					      NULL);
-	
+
+	if (filename) {
+		gtk_file_chooser_set_filename (GTK_FILE_CHOOSER (dialog), filename);
+	}
+#if 0
+	GtkFileFilter *filter = gtk_file_filter_new ();
+	gtk_file_filter_set_name (filter, "All Files");
+	gtk_file_filter_add_pattern (filter, "*");
+	gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog), filter);
+
+	GtkFileFilter *test_filter = gtk_file_filter_new ();
+	gtk_file_filter_set_name (test_filter, "My Test Filter");
+	gtk_file_filter_add_mime_type (test_filter, "text/xml");
+	gtk_file_filter_add_mime_type (test_filter, "x-directory/normal");
+	gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog), 
+				     test_filter);
+	gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (dialog), 
+				     test_filter);
+#endif
 	if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT) {
 		result = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
 	}
@@ -47,18 +70,19 @@ gchar *cong_get_file_name(const gchar *title,
 	return result;
 }
 #else
-/* Implementation in terms of legacy GtkFileSelection interface: */
-gchar *cong_get_file_name(const gchar *title, 
-			  const gchar *filename,
-			  GtkWindow *parent_window,
-			  CongFileChooserAction action)
+/* Implementation in terms of gtk_file_selection: */
+gchar*
+cong_get_file_name (const gchar *title, 
+		    const gchar *filename,
+		    GtkWindow *parent_window,
+		    CongFileChooserAction cong_action)
 {
 	GtkFileSelection *file_selection;
 	gint result_int;
 	gchar *result_filename;
 
-	g_return_val_if_fail(title, NULL);
-	g_return_val_if_fail(parent_window, NULL);
+	g_return_val_if_fail (title, NULL);
+	g_return_val_if_fail (parent_window, NULL);
 
 	/* Create a new file selection widget */
 	file_selection = GTK_FILE_SELECTION(gtk_file_selection_new (title));
