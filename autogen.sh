b4b113937ced2e4dacd17394d3577074d6f03f9e
Nickolay V. Shmyrev <nshmyrev@src.gnome.org>
2008-01-03 11:49:39 +0000

    Fixes autogen configure check
    svn path=/trunk/; revision=2161

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="Conglomerate"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/src/main.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
